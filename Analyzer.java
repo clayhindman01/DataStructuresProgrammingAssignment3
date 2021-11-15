2b645122ec90da19028891115f9783280c497784
clayhindman01 <clayhindman01@gmail.com>
2021-11-15 13:11:22 -0500

    Added functionality for the hashCode method and added a method for calculating polynomials based on horners method.

--- Analyzer.java
+++ Analyzer.java
@@ -4,16 +4,56 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Analyzer {
+
+	//Method for calculating horner method.
+	//I created this helper method
+	/**
+	 * 
+	 * @param poly array of ascii values for the string
+	 * @param increment amount that the factor will increase by
+	 * @param n factor
+	 * @return the hashed value of the poly[] array based on horners method for finding polynomials
+	 */
+	public static int horner(int poly[], int increment, int n) {
+		int result = poly[n-1];
+		int factor = 0;
+		for (int i = n-1; i >= 0; i--) {
+			result = result + factor * poly[i];
+			if (factor == 0) {
+				factor += increment;
+			} else {
+				factor *= increment;
+			}
+		}
+		return result;
+	}
+
 	//compute polynomial hash code according to page 413 in our book
 	//Use horner's method 
 	public static int hashCode(int a, String x) {
-		return 0;
+		int hash = 0;
+
+		if (a == 1) {
+			for (int i = 0; i < x.length(); i++) {
+				hash = hash + x.charAt(i);
+			}
+			return hash;
+		} else {
+			//Return different hash codes for "stop", "tops", and "pots".
+			int[] poly = new int [x.length()];
+			for (int i = 0; i < x.length(); i++) {
+				poly[i] = x.charAt(i);
+			}
+			int n = x.length();
+			return horner(poly, a, n);
+		}
 	}
 	
 	
 	public static void main(String[] args) {
-		
-		
+		System.out.println(hashCode(3, "stop"));
+
+		//TODO: Create map that will store the hashed value.
 	}
 
 }

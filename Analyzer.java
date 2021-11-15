c5998ebbce70dfa42a7a7a823098065a2f0e0609
clayhindman01 <clayhindman01@gmail.com>
2021-11-14 20:02:04 -0500

    initial commit

---
+++ Analyzer.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Analyzer {
+	//compute polynomial hash code according to page 413 in our book
+	//Use horner's method 
+	public static int hashCode(int a, String x) {
+		return 0;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
